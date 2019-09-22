cd601ffb219dc84fc836e3c6e1b2c6946464ae20
Matthew Mason <me@mtthwmsn.co.uk>
2019-09-22 14:48:04 +0100

    set up

---
+++ gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			options: {
+				separator: ';',
+			},
+			dist: {
+				src: [
+					'src/helper/*.js',
+					'src/true-smooth.js'
+				],
+				dest: 'dist/true-smooth.js',
+			}
+		},
+		uglify: {
+			build: {
+				src: ['dist/true-smooth.js'],
+				dest: 'dist/true-smooth.min.js'
+			}
+		},
+		watch: {
+			js: {
+				files: 'src/**/*.js',
+				tasks: ['concat', 'uglify']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify-es');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('dev', [
+		'concat',
+		'uglify',
+		'watch'
+	]);
+}
